9eb6de8236fc17f63f85830806a41f854bfc9178
14hydra <sarpakalin14@gmail.com>
2025-07-01 17:43:30 +0000

    hotel

---
+++ lessons/50_Projects/hotel_management.py
@@ -0,0 +1,35 @@
+class hotel_management:
+    rooms = {
+
+        "1": None,
+        "2": None,
+        "3": None,
+        "4": None,
+        "5": None,
+        "6": None,
+        "7": None,
+        "8": None,
+        "9": None,
+        "10": None
+
+    }
+
+    days = {
+
+        "1": 0,
+        "2": 0,
+        "3": 0,
+        "4": 0,
+        "5": 0,
+        "6": 0,
+        "7": 0,
+        "8": 0,
+        "9": 0,
+        "10": 0
+
+    }
+
+    waiting_list = ()
+
+    shuttle_reservation = [None, None, None, None, None]
+
